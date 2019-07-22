715b31c9822eda651a5d513630c9c686411ce17c
leigh-perry <lperry.breakpoint@gmail.com>
2019-07-22 02:15:19 +0000

    master: Comment

--- modules/lptemplateprojectname/src/main/scala/com/lptemplatecompany/lptemplatedivision/lptemplateservicename/package.scala
+++ modules/lptemplateprojectname/src/main/scala/com/lptemplatecompany/lptemplatedivision/lptemplateservicename/package.scala
@@ -2,4 +2,7 @@ package com.lptemplatecompany.lptemplatedivision
 
 package object lptemplateservicename {
 
+  // There is no AIO - EitherT is not used in this cats.effect.IO app, just plain IO since
+  // `AppError` extends `Throwable`.
+
 }
