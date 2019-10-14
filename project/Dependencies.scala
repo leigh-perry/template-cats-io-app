8665176b179bbb329547222908b855cdd91d9c21
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-14 01:01:18 +0000

    Fix compile error

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,6 +11,7 @@ object Dependencies {
     val log4cats = "1.0.0"
     val conduction = "0.4.2"
 
+    val minitest = "2.7.0"
     val scalacheck = "1.14.2"
   }
 
@@ -21,6 +22,8 @@ object Dependencies {
   val log4catsSlf4j = "io.chrisdavenport" %% "log4cats-slf4j" % Version.log4cats
   val conduction = "com.github.leigh-perry" %% "conduction-core" % Version.conduction
 
+  val minitest = "io.monix" %% "minitest" % Version.minitest
+  val minitestLaws = "io.monix" %% "minitest-laws" % Version.minitest
   val scalacheck = "org.scalacheck" %% "scalacheck" % Version.scalacheck
   val catsLaws = "org.typelevel" %% "cats-laws" % Version.cats
 }
