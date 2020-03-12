b1a1998abcc250f9bdc71278d4ca14203b081929
leigh-perry <lperry.breakpoint@gmail.com>
2020-03-12 16:57:46 +1100

    Refactor app

--- apps/lptemplateprojectname/src/main/scala/com/lptemplatecompany/lptemplatedivision/lptemplateservicename/AppMain.scala
+++ apps/lptemplateprojectname/src/main/scala/com/lptemplatecompany/lptemplatedivision/lptemplateservicename/AppMain.scala
@@ -7,28 +7,20 @@ import com.lptemplatecompany.lptemplatedivision.shared.interpreter.Info
 import io.chrisdavenport.log4cats.Logger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 
-/**
- * All resources, such as temporary directories and the expanded files, are cleaned up when no longer
- * required. This is implemented using `cats.effect.Resource`.
- */
 object AppMain extends IOApp {
-
-  private def getLogger: IO[Logger[IO]] =
-    Slf4jLogger.create[IO]
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      outcome <- program.attempt
-      log <- getLogger
+      log <- Slf4jLogger.create[IO]
+      outcome <- program(log).attempt
       code <- outcome.fold(
         e => log.error(s"Application failed: $e") *> IO(ExitCode.Error),
         _ => log.info("Application terminated with no error indication") *> IO(ExitCode.Success)
       )
     } yield code
 
-  private def program: IO[Unit] =
+  private def program(log: Logger[IO]): IO[Unit] =
     for {
-      log <- getLogger
       cfg <- Config.load
       info <- Info.of[IO, Config](cfg, log)
       _ <- info.logEnvironment
@@ -36,6 +28,10 @@ object AppMain extends IOApp {
       outcome <- runApp(cfg, log)
     } yield outcome
 
+  /**
+   * All resources, such as temporary directories and the expanded files, are cleaned up when no longer
+   * required. This is implemented using `cats.effect.Resource`.
+   */
   private def runApp(cfg: Config, log: Logger[IO]): IO[Unit] =
     Context.create(cfg, log).use {
       ctx =>
